f09ee77c10ed25d409c0beee2ca20a34a1d250bf
Markov88 <TodorMarkov88@abv.bg>
2019-11-05 21:30:28 +0200

    1234

--- app.js
+++ app.js
@@ -3,17 +3,39 @@ const minutes = document.querySelector('#minutes');
 const result = document.querySelector('#result');
 
 
-const resultX = () => {
+
+let resultX = () => {
+    if (hour.value > 0 && hour.value <= 12 && minutes.value > 0 && minutes.value <= 60) {
+
     const cornerMinutes = 360 - minutes.value * 6;
-    const cornerHour = 360 - hour.value * 30;
+    let cornerHour = 360 - hour.value * 30;
     const y = (minutes.value / 60) * 30;
-    let angle = (cornerHour - cornerMinutes - y);
-    if (angle > 180) {
-        angle = 360 - (cornerHour - cornerMinutes - y)  
+    let angle;
+
+    if (hour.value === 12) {
+        cornerHour = 0
     }
-    result.textContent = `${angle}`
+
+    if (cornerHour > cornerMinutes) {
+        angle = cornerHour - cornerMinutes - y;
+    } else if (cornerMinutes > cornerHour) {
+        angle = cornerMinutes - cornerHour + y;
+    } else {
+        angle = 0;
+    }
+    if (angle > 180) {
+        angle = 360 - angle;
+    }
+    if (angle < 0) {
+        angle = Math.abs(angle);
+    }
+
+    result.textContent = `${ angle }`;
+}
 };
 
-minutes.addEventListener('change', resultX);
-hour.addEventListener('change', resultX);
+
+
+    minutes.addEventListener('change', resultX);
+    hour.addEventListener('change', resultX);
 
