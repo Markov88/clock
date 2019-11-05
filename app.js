a1cbad9c9bbf0f7161d3cf86635e06ecbc0aafbe
Todor <todormarkoww@gmail.com>
2019-11-05 16:56:03 +0200

    zz

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const result = document.querySelector('#result');
 
 
 const resultX = () => {
-    const cornerMinutes = minutes.value * 6;
-    const cornerHour = hour.value * 30;
+    const cornerMinutes = 360 - minutes.value * 6;
+    const cornerHour = 360 - hour.value * 30;
     const y = (minutes.value / 60) * 30;
-    let angle = (cornerHour + cornerMinutes - y);
- 
+    let angle = (cornerHour - cornerMinutes - y);
+    if (angle > 180) {
+        angle = 360 - (cornerHour - cornerMinutes - y)  
+    }
     result.textContent = `${angle}`
 };
 
