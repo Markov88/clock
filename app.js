92acdff8eee2ef5441b513f75323aa611165bd74
Markov88 <TodorMarkov88@abv.bg>
2019-11-05 22:20:52 +0200

    not finished

--- app.js
+++ app.js
@@ -2,40 +2,45 @@ const hour = document.querySelector('#hour');
 const minutes = document.querySelector('#minutes');
 const result = document.querySelector('#result');
 
+const clockHour = document.querySelector('.hour');
+
 
 
 let resultX = () => {
     if (hour.value > 0 && hour.value <= 12 && minutes.value > 0 && minutes.value <= 60) {
 
-    const cornerMinutes = 360 - minutes.value * 6;
-    let cornerHour = 360 - hour.value * 30;
-    const y = (minutes.value / 60) * 30;
-    let angle;
+        const cornerMinutes = 360 - minutes.value * 6;
+        let cornerHour = 360 - hour.value * 30;
+        const y = (minutes.value / 60) * 30;
+        let angle;
 
-    if (hour.value === 12) {
-        cornerHour = 0
+        if (hour.value === 12) {
+            cornerHour = 0
+        }
+
+        if (cornerHour > cornerMinutes) {
+            angle = cornerHour - cornerMinutes - y;
+        } else if (cornerMinutes > cornerHour) {
+            angle = cornerMinutes - cornerHour + y;
+        } else {
+            angle = 0;
+        }
+        if (angle > 180) {
+            angle = 360 - angle;
+        }
+        if (angle < 0) {
+            angle = Math.abs(angle);
+        }
+
+        clockHour.style.transform = `rotate(${ cornerHour }deg)`
+
+        result.textContent = `${ angle }`;
     }
 
-    if (cornerHour > cornerMinutes) {
-        angle = cornerHour - cornerMinutes - y;
-    } else if (cornerMinutes > cornerHour) {
-        angle = cornerMinutes - cornerHour + y;
-    } else {
-        angle = 0;
-    }
-    if (angle > 180) {
-        angle = 360 - angle;
-    }
-    if (angle < 0) {
-        angle = Math.abs(angle);
-    }
-
-    result.textContent = `${ angle }`;
-}
 };
 
 
 
-    minutes.addEventListener('change', resultX);
-    hour.addEventListener('change', resultX);
+minutes.addEventListener('change', resultX);
+hour.addEventListener('change', resultX);
 
